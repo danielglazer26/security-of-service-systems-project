c8e602b808d0bb2abbd2c8586f9b8a624bbfd995
danielglazer26 <danielglazer200026@gmail.com>
2023-03-10 20:05:39 +0100

    Add Backend-React and change name of backend to Backend-SSO

---
+++ Backend-React/src/main/java/bednarz/glazer/sakowicz/BackendReactApplication.java
@@ -0,0 +1,13 @@
+package bednarz.glazer.sakowicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendReactApplication.class, args);
+	}
+
+}
