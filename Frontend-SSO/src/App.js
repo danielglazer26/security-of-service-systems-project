f1e782a8d1840fc68c89dea74e0500599e182e24
eva-m-s <88543636+eva-m-s@users.noreply.github.com>
2023-05-25 13:13:00 +0200

    feat: navigate to "/" when no return url

--- Frontend-SSO/src/App.js
+++ Frontend-SSO/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Login } from "./Login";
 import { Register } from "./Register";
 import { TempApp } from "./TempApp";
@@ -8,6 +8,31 @@ import { TempApp } from "./TempApp";
 //import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
+  const [isLoading, setIsLoading] = useState('true');
+  const urlParams = new URLSearchParams(window.location.search);
+  const returnUrl = urlParams.get("returnUrl");
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const { state } = location;
+  //   if (!state || !state.fromAnotherApp) {
+  //     navigate("/"); // Redirect to the main app page if not accessed from another app
+  //   }
+  // }, [navigate, location]);
+
+  useEffect(() => {
+    if (!returnUrl) {
+      navigate("/"); // Redirect to the main app page if returnUrl is not provided
+      setIsLoading(false)
+    }
+    else {
+      setIsLoading(false)
+    }
+  }, );
+
+  if (isLoading) {
+    return <div className='loading'>Loading...</div>; // Show loading indicator while checking authorization
+  } 
 
   return (
     <div className="App">
