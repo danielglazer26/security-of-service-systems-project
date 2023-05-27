47a25f1afca8e4c59b6316587f2d696ce53b503b
Wojciech Bednarz <w.bednarz2000@gmail.com>
2023-05-27 16:15:15 +0200

    refactor: Dev profile as default

--- start-all-dev.cmd
+++ start-all-dev.cmd
@@ -4,4 +4,4 @@ start /min cmd.exe @cmd /c "cd .\Backend-App1 && .\mvnw clean install -DskipTest
 start /min cmd.exe @cmd /c "cd .\Backend-App2 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App2.jar"
 start /min cmd.exe @cmd /c npm start --prefix .\Frontend-SSO\
 start /min cmd.exe @cmd /c npm start --prefix .\Frontend-App1\
-echo System started successfully!
+echo Starting system...
