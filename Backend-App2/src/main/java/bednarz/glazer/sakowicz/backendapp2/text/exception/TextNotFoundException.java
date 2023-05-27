90c6c5c66114802859871464cff0803b0f834810
Wojciech Bednarz <w.bednarz2000@gmail.com>
2023-05-27 04:32:30 +0200

    fix: Removed text package

--- Backend-App2/src/main/java/bednarz/glazer/sakowicz/backendapp2/text/exception/TextNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bednarz.glazer.sakowicz.backendapp2.text.exception;
-
-public class TextNotFoundException extends RuntimeException {
-    public TextNotFoundException(Long id) {
-        super("Text with id = %d does not exist".formatted(id));
-    }
-}
