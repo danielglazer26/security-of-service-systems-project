71448e217658536fe5c00110d9e4d157897b248e
Wojciech Bednarz <w.bednarz2000@gmail.com>
2023-05-27 16:06:10 +0200

    refactor: Dev profile as default

--- start-all-demo.cmd
+++ start-all-demo.cmd
@@ -1,5 +1,7 @@
 @echo off
 start /min cmd.exe @cmd /c docker compose -f .\Docker-DBs\docker-compose.yml up
+echo Starting DBs...
+timeout 5 > nul
 start /min cmd.exe @cmd /c "cd .\Backend-SSO && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-SSO.jar --spring.profiles.active=demo"
 start /min cmd.exe @cmd /c "cd .\Backend-App1 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App1.jar --spring.profiles.active=demo"
 start /min cmd.exe @cmd /c "cd .\Backend-App2 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App2.jar"
--- start-system-demo.cmd
+++ start-system-demo.cmd
@@ -1,6 +1,6 @@
 @echo off
-start /min cmd.exe @cmd /c "cd .\Backend-SSO && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-SSO.jar"
-start /min cmd.exe @cmd /c "cd .\Backend-App1 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App1.jar"
+start /min cmd.exe @cmd /c "cd .\Backend-SSO && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-SSO.jar --spring.profiles.active=demo"
+start /min cmd.exe @cmd /c "cd .\Backend-App1 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App1.jar --spring.profiles.active=demo"
 start /min cmd.exe @cmd /c "cd .\Backend-App2 && .\mvnw clean install -DskipTests -q && java -jar .\target\Backend-App2.jar"
 start /min cmd.exe @cmd /c npm start --prefix .\Frontend-SSO\
 start /min cmd.exe @cmd /c npm start --prefix .\Frontend-App1\
